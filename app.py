8b214b1aee61c7c7ba4bec67dc0cc1111baf5791
DzungHoang <DzungHoang@gitlab.com>
2019-12-11 09:44:08 +0700

    add suggestion

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request, abort
-from backend import process
+from backend import process, content2dglid
+from tqdm import tqdm 
+
 app = Flask(__name__)
+
+terms = []
+users = []
+groups = []
+
+for ntype, content in tqdm(content2dglid, desc="Read keywords"):
+   if ntype == 'term':
+      terms.append(content)
+   elif ntype == 'user':
+      users.append(content)
+   elif ntype == 'groups':
+      groups.append(content)
+
 
 @app.route('/')
 def student():
-   return render_template('home.html')
+   return render_template('home.html', terms=terms, users=users,groups=groups)
 
 @app.route('/result',methods = ['POST'])
 def result():
@@ -19,7 +34,8 @@ def result():
 
       print(result)
       data = process(result)
-      return render_template("result.html", data = data, term = term, user=user, group=group)
+      return render_template("result.html", data = data, term = term, user=user, group=group,\
+         terms=terms, users=users,groups=groups)
    else:
       abort(405)
 
